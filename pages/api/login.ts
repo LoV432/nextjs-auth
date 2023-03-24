24ae47535df9c504cc02a7ecd4a5fb049b2ab7e4
LoV432 <monib619@gmail.com>
2023-03-25 01:42:18 +0500

    add login api and page

---
+++ pages/api/login.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withSessionRoute } from '@/lib/withSession';
+
+export default withSessionRoute(loginRoute);
+
+async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === 'POST') {
+		const { username, password } = req.body;
+
+		try {
+			const response = await fetch(process.env.AUTH_LINK, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ username, password })
+			});
+			let responseJson = await response.json();
+			if (responseJson.success) {
+				req.session.user = {
+					username: responseJson.data.displayName,
+					membership: responseJson.data.membership
+				};
+				await req.session.save();
+				return res.send({ ok: true });
+			}
+		} catch (error) {
+			return res.status(403).send({ failed: true });
+		}
+		return res.status(403).send({ failed: true });
+	}
+	return res.redirect('/login');
+}
---
+++ pages/login/index.tsx
@@ -0,0 +1,30 @@
+export default function Login() {
+	async function login(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		const username = (e.currentTarget.elements[0] as HTMLInputElement).value;
+		const password = (e.currentTarget.elements[1] as HTMLInputElement).value;
+		const response = await fetch('/api/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ username, password })
+		});
+		let responseJson = await response.json();
+		if (responseJson.ok) {
+			window.location.href = '/profile';
+		}
+	}
+	return (
+		<>
+			<div className="grid h-full place-items-center">
+				<form onSubmit={login} className="grid place-items-center rounded border-2 border-solid border-black p-2">
+					<h1 className="p-5 pt-0 text-3xl font-bold">Login</h1>
+					<input type="text" placeholder="Username" required className="mb-2 w-full max-w-xs border-2 border-solid p-2" />
+					<input type="password" placeholder="Password" required className="w-full max-w-xs border-2 border-solid p-2" />
+					<button className="mt-2 rounded bg-green-600 px-5 py-2 hover:bg-green-700 hover:text-white">Login</button>
+				</form>
+			</div>
+		</>
+	);
+}
