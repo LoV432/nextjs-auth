8b71b7e658121530a797cf08b277a8bf13728234
LoV432 <monib619@gmail.com>
2023-03-25 01:42:54 +0500

    add logout api

---
+++ pages/api/logout.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withSessionRoute } from '../../lib/withSession';
+
+export default withSessionRoute(loginRoute);
+
+async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
+	req.session.destroy();
+	return res.redirect('/login');
+}
