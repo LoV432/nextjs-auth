47ba1104e9e18949011e547458a1faf11acc6c21
LoV432 <monib619@gmail.com>
2023-03-25 01:40:34 +0500

    add /profile page

---
+++ pages/profile/index.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+	return (
+		<div className="grid h-full place-items-center">
+			<h1 className="text-center text-4xl font-bold">WELCOME TO THE PROFILE</h1>
+		</div>
+	);
+}
