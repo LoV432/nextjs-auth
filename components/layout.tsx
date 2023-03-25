7fe7499e5b2bcc294341d3d35f5867412514030d
LoV432 <monib619@gmail.com>
2023-03-26 00:09:04 +0500

    refactor navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,13 @@
 import CustomHead from './head';
 import Footer from './footer';
 import Navbar from './navbar';
-import useSWR from 'swr';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-	const fetcher = (apiURL: string) => fetch(apiURL).then((res) => res.json());
-	const { data, isLoading, error } = useSWR('/api/user', fetcher);
-	if (isLoading) return <></>;
-	if (error) return <></>;
 	return (
 		<>
 			<CustomHead />
 			<div className="grid min-h-screen w-full grid-rows-[auto,1fr,auto]">
-				<Navbar user={data.user} />
+				<Navbar />
 				<div>{children}</div>
 				<Footer />
 			</div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
 import { IronSessionData } from 'iron-session';
 import Link from 'next/link';
 import { useState } from 'react';
+import useSWR from 'swr';
 
 interface IronSessionDataFinal {
 	user: {
@@ -9,7 +10,12 @@ interface IronSessionDataFinal {
 	};
 }
 
-export default function Navbar({ user }: IronSessionData) {
+export default function Navbar() {
+	const fetcher = (apiURL: string) => fetch(apiURL).then((res) => res.json());
+	const { data, isLoading, error } = useSWR('/api/user', fetcher);
+	if (isLoading) return <></>;
+	if (error) return <></>;
+	const { user }: IronSessionData = data;
 	return (
 		<div className="relative flex min-h-[3rem] w-full gap-6 bg-blue-800 px-5 text-white">
 			<Link className="text-md my-auto font-semibold text-red-600 underline hover:cursor-pointer" href="/">
@@ -29,38 +35,33 @@ function NotLoggedIn() {
 }
 
 function LoggedIn({ user }: IronSessionDataFinal) {
+	const [toggleProfile, setToggleProfile] = useState(false);
 	return (
 		<>
 			<Link className="text-md my-auto font-semibold text-red-600 underline hover:cursor-pointer" href="/profile">
 				Profile
 			</Link>
-			<Profile user={user} />
-		</>
-	);
-}
-
-function Profile({ user }: IronSessionDataFinal) {
-	const [toggleProfile, setToggleProfile] = useState(false);
-
-	return (
-		<div className="my-auto ml-auto">
-			<div onClick={() => setToggleProfile(!toggleProfile)} className="h-9 w-9 overflow-hidden rounded-full bg-green-800 hover:cursor-pointer">
-				<img src="https://avatars.dicebear.com/api/open-peeps/75370480.38706368.svg?background=%23ffffff"></img>
+			<div className="my-auto ml-auto">
+				<div onClick={() => setToggleProfile(!toggleProfile)} className="h-9 w-9 overflow-hidden rounded-full bg-green-800 hover:cursor-pointer">
+					<img src="https://avatars.dicebear.com/api/open-peeps/75370480.38706368.svg?background=%23ffffff"></img>
+				</div>
+				{toggleProfile ? (
+					<>
+						<div onClick={() => setToggleProfile(!toggleProfile)} className="absolute left-0 top-0 h-screen w-screen bg-red-800 opacity-0"></div>
+						<div id="profileDropdown" className={`absolute right-1 w-56 rounded-md bg-gray-700 p-2 pb-3`}>
+							<p className="m-3 border-b border-b-gray-300 border-opacity-40 pb-1 text-center">{user.username}</p>
+							<p className="m-3 border-b border-b-gray-300 border-opacity-40 pb-1 text-center">Membership: {user.membership}</p>
+							<div className="flex items-center">
+								<Link className="text-md w-full text-center font-semibold text-red-600 underline hover:cursor-pointer" href="/api/logout">
+									Logout
+								</Link>
+							</div>
+						</div>
+					</>
+				) : (
+					<></>
+				)}
 			</div>
-			{toggleProfile ? (
-				<>
-					<div onClick={() => setToggleProfile(!toggleProfile)} className="absolute left-0 top-0 h-screen w-screen bg-red-800 opacity-0"></div>
-					<div id="profileDropdown" className={`absolute right-1 w-56 rounded-md bg-gray-700 p-2 pb-3`}>
-						<p className="pb-1">Username: {user.username}</p>
-						<p className="pb-1">Membership: {user.membership}</p>
-						<Link className="text-md font-semibold text-red-600 underline hover:cursor-pointer" href="/api/logout">
-							Logout
-						</Link>
-					</div>
-				</>
-			) : (
-				<></>
-			)}
-		</div>
+		</>
 	);
 }
