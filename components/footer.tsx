c643a9b40585db7eb02897ce8b18ec8bb0b655fb
LoV432 <monib619@gmail.com>
2023-03-26 20:16:13 +0500

    remove hardcoded footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-	return <div className="pl-3 pb-2">Chloe&apos;s Auth 2023</div>;
+	return (
+		<div className="pl-3 pb-2">
+			&copy; {new Date().getFullYear()} {process.env.NEXT_PUBLIC_COPYRIGHT_NAME}. All rights reserved.
+		</div>
+	);
 }
--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,6 @@ namespace NodeJS {
 		IRON_PASSWORD: string;
 		IRON_COOKIE_NAME: string;
 		AUTH_LINK: string;
+		NEXT_PUBLIC_COPYRIGHT_NAME: string;
 	}
 }
